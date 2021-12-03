9b9358c2b9b434c54f427b3546fcb396e5c1236f
JaredMillsap <jmillsap23@gmail.com>
2021-12-03 10:57:51 -0600

    new

---
+++ LINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>() { "NBA2K22", "NFL2K22", "NCAA 22", } ;
+
+            IEnumerable<String> games = videoGames.Where(games.OrderByDescending()
+            foreach (var Game in videoGames)
+            {
+                Console.WriteLine(Game);
+            }
+
+            var GameLength = videoGames.Contains(" ");
+             `
+            Console.WriteLine(GameLength);
+
+
+
+
+            //var namelength = videoGames.Where(x => videoGames.Count == 4);
+
+            //Console.WriteLine(namelength);
+
+            //newList = list.OrderByDescending(x => x.Product.Name).ToList();
+        }
+        //OrderByDescending(x => x).ToList();
+    }
+}
